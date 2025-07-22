83ac6e7714ce23c7e9a1a588eda4fb5c43b8b483
cdvdev <cdv1982.dev@gmail.com>
2025-07-21 22:03:34 -0300

    urlbase changed2

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:8080/api",
-  baseURL: "https://mern-crud-auth-be.onrender.com",
+  baseURL: "https://mern-crud-auth-be.onrender.com/api",
   withCredentials: true,
 });
 
